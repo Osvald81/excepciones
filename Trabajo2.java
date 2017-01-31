9b40162670034c0575aeeebed2c6b47225699433
Osvald81 <Osvald81@users.noreply.github.com>
2017-01-31 10:00:34 -0700

    Add files via upload

---
+++ Trabajo2.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo2;
+
+
+import java.awt.print.PageFormat;
+import java.util.Random;
+
+/**
+ *
+ * @author Osvaldo Alvarez
+ */
+public class Trabajo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      //Nullpointerexception//
+        
+        /*  String cadena = null;
+        System.out.println(cadena.length());
+        int a= 10/0;*/
+       /*int a= 10/0;*/
+       
+       //Arrayindexoutofboundsexception//
+       /*Random r=new Random();
+       int a[] = new int[50];
+       
+        for (int i=0; i<a.length; i++){
+            a[i]=r.nextInt(50);}
+        System.out.println(a[52]);
+        
+        for (int i : a)
+            System.out.println(i);*/
+       //ClassCastException//
+       /*A a1=new A();
+       B b1=(B)a1;*/
+       
+       //Ilegal argument//
+       /*methodf();
+    }
+       private static void methodf(){
+           PageFormat jummmm = new PageFormat();
+           jummmm.setOrientation(10);*/
+       } 
+        }
+
+
+
+ 
+       
+    }
+    
+    
+
